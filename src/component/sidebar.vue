a1ee42b69966d5da513ccdb50d987a4e5160b34d
yuan <yuan.qiujuan@trs.com.cn>
2016-12-15 18:18:45 +0800

    初始块

--- src/component/sidebar.vue
+++ src/component/sidebar.vue
@@ -2,26 +2,44 @@
 
 <template lang="html">
     <div class="flex-1 side">
-        <header class="flex row-reverse">
-            <span v-if="isAuth">
-                <i class="fa fa-envelope-o"></i>
+        <header class="flex">
+            <div class="input-group flex-3">
+                <input type="search" name="word" />
+                <span>
+                    <button class="btn" type="button">
+                        <i class="fa fa-search cadetblue"></i>
+                    </button>
+                </span>
+            </div>
+            <span class="flex-1" v-if="isAuth">
+                <i class="fa fa-bell"></i>
+                <i>13</i>
             </span>
         </header>
-        <section class="flex justify-center align-center flex-column">
-            <div class="profile flex-1">
-                <img v-if="profileUrl"/>
-                <i class="fa fa-user-o" v-else></i>
+        <div class="modules">
+            <div class="fun" :class="{'small-modules': isShowDetail}">
+                <a v-for="item in modules"
+                   :style="{backgroundColor: item.bgColor}"
+                   v-on:click="getModuleDetail(item.enName)">
+                    {{item.name}}
+                </a>
             </div>
-            <div class="contact">
+            <div class="modules-detail" v-if="isShowDetail">
+                <label></label>
+            </div>
+        </div>
+        <section class="flex">
+            <div class="flex flex-1 row-reverse">
+                <div class="profile">
+                    <img v-if="profileUrl"/>
+                    <i class="fa fa-user-o" v-else></i>
+                </div>
+            </div>
+            <div class="contact flex-2">
                 <p>名字：Choojen</p>
                 <p>Email：qiujuan.yuan@163.com</p>
             </div>
         </section>
-        <div class="fun flex">
-            <a v-for="item in games" class="flex-1">
-                {{item.name}}
-            </a>
-        </div>
     </div>
 </template>
 
@@ -33,24 +51,13 @@
                 name: "test2",
                 isAuth: true,
                 profileUrl: false,
-                games: [
-                    {
-                        name: "五子棋",
-                        enName: "gobang"
-                    },
-                    {
-                        name: "俄罗斯方块",
-                        enName: "elous"
-                    },
-                    {
-                        name: "神仙道",
-                        enName: "shenxiandao"
-                    },
-                    {
-                        name: "拼图",
-                        enName: "pintu"
-                    }
-                ]
+                modules: Config.modules,
+                isShowDetail: false
+            }
+        },
+        methods: {
+            getModuleDetail: function(key){
+                this.isShowDetail = true
             }
         }
     }
@@ -59,6 +66,7 @@
 <style rel="stylesheet/sass" lang="sass?indentedSyntax">
     @import "../../sass/define"
     $headerH: 40px
+    $funH: 250px
 
     .side
         padding: $commonGap
@@ -67,24 +75,50 @@
         height: $headerH
         line-height: $headerH
         box-sizing: border-box
+        text-align: right
+
+        .input-group
+            input
+                width: 260px
+
         span
             i.fa
                 font-size: 1.6rem
+                color: #e9967a
+
+    .modules
+        height: 24rem
+        width: 24rem
+        margin: $headerH auto
+
+        .fun
+            height: 100%
+            a
+                display: inline-block
+                width: 50%
+                height: 50%
+                &:hover
+                    cursor: pointer
+
+        .small-modules
+            height: 50px
+
 
     section
+        margin: $headerH 0
         .profile
-            width: 100px
-            height: 100px
+            width: 80px
+            height: 80px
             border-radius: 50%
-            background-color: #eeeeee
+            background-color: #FF8C00
             text-align: center
-            line-height: 100px
+            line-height: 80px
+            margin-right: $commonGap
             i
-                font-size: 8rem
+                font-size: 5rem
                 color: #fff
                 vertical-align: middle
 
         .contact
             font-size: 1.4rem
-
 </style>
---
+++ src/config.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Choojen on 2016/12/15.
+ */
+'use strict';
+var Config = {
+    modules: [
+        {
+            name: "技术",
+            enName: "code",
+            bgColor: "#5f9ea0"
+        },
+        {
+            name: "游戏",
+            enName: "games",
+            bgColor: "#ffb6c1"
+        },
+        {
+            name: "杂谈",
+            enName: "talk",
+            bgColor: "#9acd32"
+        },
+        {
+            name: "我的",
+            enName: "mine",
+            bgColor: "#e9967a"
+        }
+    ]
+};
